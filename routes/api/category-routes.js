9ffc71537f5dc40ea4cbf887b447c9bbb63f46b1
Emily McLean <emilymclean93@gmail.com>
2023-05-03 08:34:19 -0400

    coded tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-  // find all categories
-  // be sure to include its associated Products
+// find all categories and include associated products
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
@@ -16,9 +15,8 @@ router.get('/', async (req, res) => {
   }
 });
 
+// find one category by its `id` value and include its associated Products
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -35,8 +33,8 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
